f2f91e4c03e5075b2fe988e5b0c441bce672eb99
Odeinjul <odeinjul@gmail.com>
2023-07-17 00:01:56 +0800

    Add buttons

--- Pomodomo/PomodomoView.swift
+++ Pomodomo/PomodomoView.swift
@@ -15,13 +15,13 @@ struct CircularProgressView: View {
                 .trim(from: 0, to: 0.5)
                 .stroke(
                     Color("Gray"),
-                    style: StrokeStyle(lineWidth: 5, lineCap: .round)
+                    style: StrokeStyle(lineWidth: 8, lineCap: .round)
                 )
             Circle()
-                .trim(from: 0, to: progress)
+                .trim(from: 0, to: progress / 2)
                 .stroke(
                     Color.accentColor,
-                    style: StrokeStyle(lineWidth: 5, lineCap: .round)
+                    style: StrokeStyle(lineWidth: 8, lineCap: .round)
             )
         }
         .rotationEffect(.degrees(180))
@@ -34,20 +34,67 @@ func secondsToHoursMinutesSeconds(_ seconds: Int) -> (String) {
     return (m < 10 ? "0" : "") + "\(m)" + (s < 10 ? ":0" : ":") + "\(s)"
 }
 
+struct BlackButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(.vertical, 10)
+            .padding(.horizontal, 10)
+            .background(Color(red: 0, green: 0, blue: 0))
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
+    }
+}
+
+
 struct TimerView: View {
     @State var timePassed = 0.0
     @State var timeTotal = 25 * 60.0
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
         ZStack {
-            Text("\(secondsToHoursMinutesSeconds(Int(timeTotal - timePassed)))")
-                .onReceive(timer) { _ in
-                    if timePassed < timeTotal {
-                        timePassed += 1.0
-                    }
+            VStack (spacing: 25) {
+                VStack (spacing: 0) {
+                    Text("剩余时间")
+                        .font(.system(size: 15, weight: .semibold))
+                    Text("\(secondsToHoursMinutesSeconds(Int(timeTotal - timePassed)))")
+                        .onReceive(timer) { _ in
+                            if timePassed < timeTotal {
+                                timePassed += 1.0
+                            }
+                        }
+                        .font(.system(size: 55))
+                    Text("（目标：25分钟）")
+                        .font(.system(size: 13, weight: .regular))
                 }
-                .font(.system(size: 60))
-                .offset(y: -23)
+                
+                HStack {
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName:"backward.end.circle")
+                    }
+                    .buttonStyle(BlackButton())
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("暂停")
+                            .padding(.vertical, 3)
+                            .padding(.horizontal, 20)
+                            .font(.system(size: 14, weight: .medium))
+                    }
+                    .buttonStyle(BlackButton())
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName:"arrow.forward.circle")
+                    }
+                    .buttonStyle(BlackButton())
+                }
+                .font(.system(size: 20, weight: .medium))
+            }
+            .offset(y: -80)
             
             CircularProgressView(progress: timePassed/timeTotal)
         }
